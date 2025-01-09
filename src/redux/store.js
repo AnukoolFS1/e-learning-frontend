fc6af796fad52f268f618c5998668b19b7f833a9
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-01-09 18:23:41 +0530

    Provider wrap to app and reducers at store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import batchReducer from "./batchesSlice";
+import courseReducer from "./courseSlice";
 
 const store = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        batches: batchReducer,
+        courses: courseReducer 
     }
 })
 
