15851ee2514396e61eea8c6fbde8f56710aa2fcc
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-01-11 16:55:17 +0530

    testing

--- src/components/dashboard/Dashboards.jsx
+++ src/components/dashboard/Dashboards.jsx
@@ -15,19 +15,22 @@ const Dashboard = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         async function fetchData (){
-            const response = await fetch('https://e-learning-backend-11lu.onrender.com/',{
+            const response = await fetch('https://e-learning-backend-11lu.onrender.com',{
                 method: "GET",
                 credentials: "include"
             })
 
-            if(!response.ok) return navigate('/signin')
-            const data = await response.json();
-            dispatch(addUser(data.user));
-            dispatch(addBatches(data.batches));
-            dispatch(addCourses(data.courses));
-            dispatch(addStudents(data.students)); 
-            dispatch(addMaterials(data.studyMaterials))
-            dispatch(addInstructor(data.instructors)); 
+            if(!response.ok){
+                 return navigate('/signin')
+            }else{
+                const data = await response.json();
+                dispatch(addUser(data.user));
+                dispatch(addBatches(data.batches));
+                dispatch(addCourses(data.courses));
+                dispatch(addStudents(data.students)); 
+                dispatch(addMaterials(data.studyMaterials))
+                dispatch(addInstructor(data.instructors)); 
+            }
         }
         fetchData()
     }, [])
